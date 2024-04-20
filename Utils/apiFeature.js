fb785775761e5dad22c406e2b4d631a64020d474
khanef <vlpkhang.20it9@vku.udn.vn>
2024-04-21 00:13:42 +0700

    complete API Feature

---
+++ Utils/apiFeature.js
@@ -0,0 +1,67 @@
+
+import { ethers } from "ethers";
+import Web3modal from 'web3modal';
+
+import {ChatAppAddress, ChatAppABI} from '../Context/Constants'
+
+export const CheckIfWalletConnected = async () => {
+    try {
+        if(!window.ethereum) return console.log("Install Metamask");
+
+        const accounts = await window.ethereum.request({
+            method: "eth_accounts",
+        })
+        const firstAccount = accounts[0];
+        return firstAccount;
+    } catch (error) {
+        console.log("Install Metamask")
+    }
+}
+
+export const ConnectWallet = async () => {
+    try {
+        if(!window.ethereum) return console.log("Install Metamask");
+
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        })
+        const firstAccount = accounts[0];
+        return firstAccount;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const fetchContract = (signerOrProvider) => new ethers.Contract(ChatAppABI, ChatAppAddress, signerOrProvider);
+
+export const connectingWithContract = async () => {
+    try {
+        const web3modal = new Web3modal();
+        const connection = await web3modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = fetchContract(signer);
+
+        return contract;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const converTime = (time) => {
+    const newTime = newData(time.toNumber());
+
+    const realTime = 
+        newTime.getHours() + 
+        "/" + 
+        newTime.getMinutes() + 
+        "/" + 
+        newTime.getSeconds() + 
+        " Date:" + 
+        newTime.getDate() + 
+        "/" + 
+        (newTime.getMonth() + 1) + 
+        "/" + 
+        newTime.getFullYear();
+    return realTime;
+}
