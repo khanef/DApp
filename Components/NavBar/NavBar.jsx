35cbea1199ab5e7b3ec74fc4bc37b48d5b260138
khanef <vlpkhang.20it9@vku.udn.vn>
2024-04-26 18:09:36 +0700

    complete model component

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -40,7 +40,7 @@ const NavBar = () => {
   const [open, setOpen] = useState(false)
   const [openModel, setOpenModel] = useState(false)
 
-  const {account, userName, connectWallet } = useContext(ChatAppContect)
+  const {account, userName, connectWallet, createAccount } = useContext(ChatAppContect)
 
   return (
     <div className={Style.NavBar}>
@@ -108,6 +108,25 @@ const NavBar = () => {
           </div>
         </div>
       </div>
+
+      {/*MODEL COMPONENT*/}
+
+      {openModel && (
+        <div className={Style.modelBox}>
+          <Model 
+            openBox={setOpenModel} 
+            title="WELCOME TO" 
+            head="CHAT BUDDY" 
+            info="lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            smallInfo="Kindly seclet your name...."
+            image={images.hero}
+            functionName={createAccount}
+            address={account}
+          />
+        </div>
+      )}
+
+      {Error == "" ? "" : <Error error={Error}/>}
     </div>
   )
 }
